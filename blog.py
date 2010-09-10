b1382d6962bc33399ab46605b397992e9df3274c
Aleksey Rybalkin <alekseyrybalkin@gmail.com>
2010-09-10 14:47:08 +0400

    blog pagination added, 5 posts per page hardcoded

--- blog.py
+++ blog.py
@@ -26,10 +26,28 @@ class ViewBlog(webapp.RequestHandler):
         if not loluser:
             self.redirect('/notfound')
             return
+        if (self.request.get('page')):
+            try:
+                intpage = int(self.request.get('page'))
+            except ValueError:
+                self.redirect('/notfound')
+                return
+        else:
+            intpage = 0
+        posts_per_page = 5
+        total_posts = db.GqlQuery('select * from Post where userid = :1 order by' +
+                                  ' date_posted desc',
+                                  int(userid)).count()
         posts = db.GqlQuery('select * from Post where userid = :1 order by' +
-                            ' date_posted desc',
+                            ' date_posted desc limit ' + str(posts_per_page) + ' offset '
+                            + str(intpage*posts_per_page),
                             int(userid))
-        values = { 'posts' : posts, 'owner' : loluser }
+        nextpage = intpage+1
+        prevpage = intpage-1
+        if total_posts <= intpage*posts_per_page + posts_per_page:
+            nextpage = -1
+        values = { 'posts' : posts, 'owner' : loluser, 'page' : str(intpage),
+                   'nextpage' : str(nextpage), 'prevpage' : str(prevpage) }
         values.update(helper.values(self.request.uri))
         path = os.path.join(os.path.dirname(__file__), 'blog.html')
         self.response.out.write(template.render(path, values))
@@ -53,16 +71,14 @@ class EditPost(webapp.RequestHandler):
     def get(self):
         user = users.get_current_user()
         loluser = helper.loluser_by_google_user_id(user.user_id())
-        if not helper.check_for_loluser(self):
-            return
+        if not helper.check_for_loluser(self): return
         if self.request.get('key'):
-            post = db.get(db.Key(self.request.get("key")))
-            if not post:
-                self.redirect("/notfound")
+            try:
+                post = db.get(db.Key(self.request.get("key")))
+            except BadKeyError:
+                self.redirect('/notfound')
                 return
-            if post.userid != loluser.id:
-                self.redirect("/accessdenied")
-                return
+            if not helper.check_for_existence_and_ownership(post, self): return
             values = { 'post' : post }
         else:
             values = {}
@@ -72,10 +88,8 @@ class EditPost(webapp.RequestHandler):
 
 class SavePost(webapp.RequestHandler):
     def post(self):
-        user = users.get_current_user()
-        loluser = helper.loluser_by_google_user_id(user.user_id())
-        if not helper.check_for_loluser(self):
-            return
+        loluser = helper.loluser_by_google_user_id(users.get_current_user().user_id())
+        if not helper.check_for_loluser(self): return
         if self.request.get('key'):
             try:        
                 post = db.get(db.Key(self.request.get("key")))
